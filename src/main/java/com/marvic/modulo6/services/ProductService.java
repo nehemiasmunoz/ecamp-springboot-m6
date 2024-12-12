aa6874bf82bf422eb252894c66d5151a15f19424
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-12 15:31:59 -0300

    Cambio de tipo de collecion a un tipo mutable Nuevo metodo para guardar producto

--- src/main/java/com/marvic/modulo6/services/ProductService.java
+++ src/main/java/com/marvic/modulo6/services/ProductService.java
@@ -4,22 +4,32 @@ import com.marvic.modulo6.dtos.ProductDTO;
 import com.marvic.modulo6.models.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
 
-    private List<Product> products = List.of(
-            new Product(1, "Laptop", "Laptop gaming de alto rendimiento", 1500.0, true),
-            new Product(2, "Smartphone", "Último modelo con tecnología 5G", 800.0, false),
-            new Product(3, "Teclado", "Teclado mecánico retroiluminado", 100.0, true)
-    );
+    ArrayList<Product> products = new ArrayList<>();
 
-    public List<ProductDTO> getAllProducts(){
+    ProductService() {
+        products.add(new Product(1, "Laptop", "Laptop gaming de alto rendimiento", 1500.0, 12));
+        products.add(new Product(2, "Smartphone", "Último modelo con tecnología 5G", 800.0, 1));
+        products.add(new Product(3, "Teclado", "Teclado mecánico retroiluminado", 100.0, 0));
+
+    }
+
+
+    public List<ProductDTO> getAllProducts() {
         //Convertimos nuestros productos a productos dto
         return products.stream()
-                .map(Product::toDTO )
+                .map(Product::toDTO)
                 .collect(Collectors.toList());
     }
+
+    public void saveProduct(Product product) {
+        products.add(product);
+    }
 }
+

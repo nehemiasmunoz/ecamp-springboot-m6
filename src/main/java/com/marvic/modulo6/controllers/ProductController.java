34c978d62e4334788cbefb080bc4f1b0e035efdb
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-12 15:40:35 -0300

    Agregados nuevos metodos para manejo de formularios

--- src/main/java/com/marvic/modulo6/controllers/ProductController.java
+++ src/main/java/com/marvic/modulo6/controllers/ProductController.java
@@ -1,15 +1,19 @@
 package com.marvic.modulo6.controllers;
 
+import com.marvic.modulo6.dtos.product.NewProductDTO;
+import com.marvic.modulo6.models.Product;
 import com.marvic.modulo6.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-    private ProductService productService;
+    private final ProductService productService;
 
     //Inyeccion por controllador
     ProductController(ProductService productService) {
@@ -22,6 +26,19 @@ public class ProductController {
         model.addAttribute("products", productService.getAllProducts());
         //Retornamos nuestra vista (products.html) en conjunto con el model y sus atributos
         return "products";
+    }
+
+    @GetMapping("/form")
+    public String getProductForm(Model model) {
+        model.addAttribute("productDTO", NewProductDTO.class);
+        return "product-form";
+    }
+
+    @PostMapping("/form")
+    public String productForm(@ModelAttribute() NewProductDTO product) {
+        productService.saveProduct(new Product().fromDTO(product));
+        //Redireccion a ruta pre establecida luego de guardar
+        return "redirect:/products/";
     }
 
 }
